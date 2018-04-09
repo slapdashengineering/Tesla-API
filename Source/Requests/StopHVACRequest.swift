874c01187fe1ce840c6f88997178ed9287a1c908
Stephen Galinsky <galinsky@gmail.com>
2018-04-09 13:06:59 -0700

    Adding stop file, whoops

---
+++ Source/Requests/StopHVACRequest.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+public struct StopHVACRequest: RequestProtocol {
+    typealias CompletionType = Bool
+    var path: String {
+        return "/api/1/vehicles/\(vehicleIdentifier)/command/auto_conditioning_stop"
+    }
+    let method = WebRequest.RequestMethod.post
+    let accessToken: String
+    let vehicleIdentifier: String
+    
+    public init(accessToken: String, vehicleIdentifier: String) {
+        self.accessToken = accessToken
+        self.vehicleIdentifier = vehicleIdentifier
+    }
+    
+    public func execute(completion: @escaping (Result<Bool>) -> Void) {
+        WebRequest.request(
+            path: path,
+            method: method,
+            accessToken: accessToken) { response, error in
+                if let error = error {
+                    DispatchQueue.main.async {
+                        completion(Result.failure(error))
+                    }
+                } else if let response = response as? [String: [String: Any]],
+                    let resultBool = response["response"]?["result"] as? Bool {
+                    DispatchQueue.main.async {
+                        completion(Result.success(resultBool))
+                    }
+                } else {
+                    DispatchQueue.main.async {
+                        completion(Result.failure(APIError()))
+                    }
+                }
+        }
+    }
+}
